cb22bcf82d72d12aeea27f62f766a63b11a2f2b5
Adam Tilly <tillyjadam@gmail.com>
2021-10-23 13:03:08 -0500

    added Manager Test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,23 @@
-test('tests creating a Manager'),() =>{
-    const employee = new employee('name','id','email','officeNumber');
+const exp = require("constants")
+const Manager = require('../lib/Manager');
 
-    expect(employee.name).toBe('name');
-    expect(employee.id).toBe('id');
-    expect(employee.email).toBe('email');
+test('tests creating an Employee',() =>{
+    const Manager = new Manager('Ed', '0015681' ,'email@gmail');
 
-    expect(employee.getName()).toEqual(expect.any(String));
-    expect(employee.getEmail()).toEqual(expect.any(String));
-    expect(employee.getId()).toEqual(expect.any(Number));
-}
+    expect(Manager.name).toEqual(expect.any(String));
+    expect(Manager.id).toEqual(expect.any(String));
+    expect(Manager.email).toEqual(expect.any(String));
+    expect(Manager.role).toEqual('Manager');
+    expect(Manager.officeNumber).toEqual(expect.any(String));
+    
+    console.table(Manager)
+
+    expect(Manager.getName()).toEqual(expect.any(String));
+    console.log(Manager.getName())
+    expect(Manager.getEmail()).toEqual(expect.any(String));
+    console.log(Manager.getEmail())
+    expect(Manager.getId()).toEqual(expect.any(String));
+    console.log(Manager.getId())
+    expect(Manager.getRole()).toEqual(expect.any(String));
+    console.log(employee.getRole())
+})
