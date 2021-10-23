3257b8ab1c3361655d09d4194cab0fefab1ed091
Adam Tilly <tillyjadam@gmail.com>
2021-10-23 12:23:24 -0500

    check in

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,10 +2,10 @@ const exp = require("constants")
 const Employee = require('../lib/Employee');
 
 test('tests creating an Employee',() =>{
-    const employee = new Employee('Ed','015681','email@gmail');
+    const employee = new Employee('Ed', 0015681 ,'email@gmail');
 
     expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
     expect(employee.role).toEqual('Employee');
 
@@ -13,7 +13,7 @@ test('tests creating an Employee',() =>{
     console.log(employee.getName())
     expect(employee.getEmail()).toEqual(expect.any(String));
     console.log(employee.getEmail())
-    expect(employee.getId()).toEqual(expect.any(String));
+    expect(employee.getId()).toEqual(expect.any(Number));
     console.log(employee.getId())
     expect(employee.getRole()).toEqual(expect.any(String));
     console.log(employee.getRole())
