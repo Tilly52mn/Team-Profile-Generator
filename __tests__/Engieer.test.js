b7c704fa79b3a71217f1391a29a11358b57daee1
Adam Tilly <tillyjadam@gmail.com>
2021-10-23 21:02:01 -0500

    added Engineer test

---
+++ __tests__/Engieer.test.js
@@ -0,0 +1,24 @@
+const exp = require("constants")
+const Manager = require('../lib/Engineer');
+
+test('tests creating an Manager',() =>{
+    const engineer = new Engineer('Ed', '0015681' ,'email@gmail','B52');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(String));
+    expect(engineer.email).toEqual(expect.any(String));
+    expect(engineer.role).toEqual('Engineer');
+    
+    console.table(engineer)
+
+    expect(engineer.getName()).toEqual(expect.any(String));
+    console.log(engineer.getName())
+    expect(engineer.getEmail()).toEqual(expect.any(String));
+    console.log(engineer.getEmail())
+    expect(engineer.getId()).toEqual(expect.any(String));
+    console.log(engineer.getId())
+    expect(engineer.getRole()).toEqual(expect.any(String));
+    console.log(engineer.getRole())
+    expect(engineer.getGithub()).toEqual(expect.any(String));
+    console.log(engineer.getGithub())
+})
