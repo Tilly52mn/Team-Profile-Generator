7ad369980726b200b8c2d2e7ff86ff5dc036343e
Adam Tilly <tillyjadam@gmail.com>
2021-10-23 20:19:27 -0500

    commit for learning assistant help

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,8 @@
 const exp = require("constants")
 const Manager = require('../lib/Manager');
 
-test('tests creating an Employee',() =>{
-    const Manager = new Manager('Ed', '0015681' ,'email@gmail');
+test('tests creating an Manager',() =>{
+    const Manager = new Manager('Ed', '0015681' ,'email@gmail','B52');
 
     expect(Manager.name).toEqual(expect.any(String));
     expect(Manager.id).toEqual(expect.any(String));
@@ -19,5 +19,5 @@ test('tests creating an Employee',() =>{
     expect(Manager.getId()).toEqual(expect.any(String));
     console.log(Manager.getId())
     expect(Manager.getRole()).toEqual(expect.any(String));
-    console.log(employee.getRole())
+    console.log(Manager.getRole())
 })
